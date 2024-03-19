60871b48f4bc1aa795f46283f543d3eb127508d8
journnie <jhyun095@gmail.com>
2024-03-19 16:58:22 +0900

    tables for Books

---
+++ create_table.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Categories (
+  category_id INT PRIMARY KEY AUTO_INCREMENT,
+  category_name VARCHAR(255) NOT NULL,
+  category_index INT
+);
+
+CREATE TABLE Authors (
+  author_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255),
+  age INT
+);
+
+CREATE TABLE Books (
+  book_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  pub_year DATETIME NOT NULL,
+  price INT NOT NULL,
+  category_id INT NOT NULL,
+  author_id INT NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES Categories(category_id),
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Users (
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  address VARCHAR(255),
+  age INT
+);
+
+CREATE TABLE Purchases (
+  purchase_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  purchase_date DATETIME NOT NULL,
+  quantity INT NOT NULL,
+  purchase_price INT NOT NULL,
+  user_id INT NOT NULL,
+  book_id INT NOT NULL,
+  Foreign Key (user_id) REFERENCES Users(user_id),
+  Foreign Key (book_id) REFERENCES Books(book_id)
+);
